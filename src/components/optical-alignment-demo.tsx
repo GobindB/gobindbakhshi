c8bfa0a14fc09fac2e074fcceb8e915d87407f92
Gobind Bakhshi <45723248+GobindB@users.noreply.github.com>
2026-04-17 22:33:29 -0700

    feat: add optical vs geometric alignment demo

---
+++ src/components/optical-alignment-demo.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+export function OpticalAlignmentDemo() {
+  return (
+    <>
+      <div className="mt-6 shrink-0 overflow-hidden rounded-xl shadow-[0_0_0_1px_#ffffff14]">
+        <div className="flex w-full shrink-0 flex-col items-center justify-center bg-[#080808] sm:flex-row">
+          {/* Geometric */}
+          <div className="flex h-72 w-full flex-col items-center justify-center gap-4 border-b border-[#1e1e1e] sm:border-b-0 sm:border-r">
+            <div className="mb-2 flex h-7 w-fit select-none items-center justify-center whitespace-nowrap rounded-full border border-[#2a2a2a] bg-[#0e0e0e] px-2.5 font-mono text-sm font-medium text-[#7c7c7c]">
+              geometric
+            </div>
+            <button className="relative flex h-12 w-12 items-center justify-center rounded-full bg-[#eee]">
+              {/* Play icon centered geometrically */}
+              <svg className="size-5 text-[#080808]" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M8 5v14l11-7z" />
+              </svg>
+              {/* Crosshair guides */}
+              <span className="absolute left-1/2 top-0 h-full w-px -translate-x-1/2 bg-red-500/30" />
+              <span className="absolute top-1/2 left-0 h-px w-full -translate-y-1/2 bg-red-500/30" />
+            </button>
+            <div className="flex size-7 items-center justify-center rounded-full bg-red-500/10">
+              <svg className="size-5 text-red-500" viewBox="0 0 24 24" fill="none">
+                <path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM9.70711 8.29289C9.31658 7.90237 8.68342 7.90237 8.29289 8.29289C7.90237 8.68342 7.90237 9.31658 8.29289 9.70711L10.5858 12L8.29289 14.2929C7.90237 14.6834 7.90237 15.3166 8.29289 15.7071C8.68342 16.0976 9.31658 16.0976 9.70711 15.7071L12 13.4142L14.2929 15.7071C14.6834 16.0976 15.3166 16.0976 15.7071 15.7071C16.0976 15.3166 16.0976 14.6834 15.7071 14.2929L13.4142 12L15.7071 9.70711C16.0976 9.31658 16.0976 8.68342 15.7071 8.29289C15.3166 7.90237 14.6834 7.90237 14.2929 8.29289L12 10.5858L9.70711 8.29289Z" fill="currentColor" />
+              </svg>
+            </div>
+          </div>
+
+          {/* Optical */}
+          <div className="flex h-72 w-full flex-col items-center justify-center gap-4">
+            <div className="mb-2 flex h-7 w-fit select-none items-center justify-center whitespace-nowrap rounded-full border border-[#2a2a2a] bg-[#0e0e0e] px-2.5 font-mono text-sm font-medium text-[#7c7c7c]">
+              optical
+            </div>
+            <button className="relative flex h-12 w-12 items-center justify-center rounded-full bg-[#eee]">
+              {/* Play icon nudged 1px right for optical center */}
+              <svg className="size-5 translate-x-[1px] text-[#080808]" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M8 5v14l11-7z" />
+              </svg>
+              <span className="absolute left-1/2 top-0 h-full w-px -translate-x-1/2 bg-green-500/30" />
+              <span className="absolute top-1/2 left-0 h-px w-full -translate-y-1/2 bg-green-500/30" />
+            </button>
+            <div className="flex size-7 items-center justify-center rounded-full bg-green-500/10">
+              <svg className="size-5 text-green-500" viewBox="0 0 24 24" fill="none">
+                <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM15.774 10.1333C16.1237 9.70582 16.0607 9.0758 15.6332 8.72607C15.2058 8.37635 14.5758 8.43935 14.226 8.86679L10.4258 13.5116L9.20711 12.2929C8.81658 11.9024 8.18342 11.9024 7.79289 12.2929C7.40237 12.6834 7.40237 13.3166 7.79289 13.7071L9.79289 15.7071C9.99267 15.9069 10.2676 16.0129 10.5498 15.9988C10.832 15.9847 11.095 15.8519 11.274 15.6333L15.774 10.1333Z" fill="currentColor" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p className="mt-4 select-none text-center text-[13px] text-[#7c7c7c]">
+        Asymmetric shapes like play icons need a small offset to appear centered to the human eye.
+      </p>
+    </>
+  );
+}
